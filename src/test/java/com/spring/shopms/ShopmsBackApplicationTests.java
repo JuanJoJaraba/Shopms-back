afdd6508a2800d17a6e766297fba55e651fa8f11
danieljaraba <daesjaga2003@gmail.com>
2023-09-07 22:51:02 -0500

    Initializing with Spring Initializr

---
+++ src/test/java/com/spring/shopms/ShopmsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.shopms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
